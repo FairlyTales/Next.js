4dfeec32c23c017f745da479ee611eadce0e08a9
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-02-05 17:26:44 +0300

    switched to typescript

---
+++ 01 - Routing/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = (props): JSX.Element => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ 01 - Routing/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {JSX} from "@babel/types";
+
+const HomePage = (props): JSX.Element => {
+	return (
+		<div>
+			<h1>Main page</h1>
+		</div>
+	);
+};
+
+export default HomePage;

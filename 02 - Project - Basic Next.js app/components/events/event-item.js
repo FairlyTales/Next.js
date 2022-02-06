bc4ae72b1b5f406ede2b7a7efcfe6ab665f1cf93
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-02-06 16:28:16 +0300

    finished project 1 - pretty basic next.js stuff

---
+++ 02 - Project - Basic Next.js app/components/events/event-item.js
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import classes from './event-item.module.css';
+
+function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = location.replace(', ', '\n');
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <Link href={exploreLink}>
+        <img src={'/' + image} alt={title} className={classes.itemLink} />
+      </Link>
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <Link href={exploreLink}>
+            <h2 className={classes.itemLink}>{title}</h2>
+          </Link>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default EventItem;

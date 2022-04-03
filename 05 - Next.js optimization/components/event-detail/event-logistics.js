a63046279408ec7d11ee5fc13cf43872a1d02546
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-04-04 01:16:01 +0300

    added next.js Link components

--- 05 - Next.js optimization/components/event-detail/event-logistics.js
+++ 05 - Next.js optimization/components/event-detail/event-logistics.js
@@ -1,22 +1,35 @@
-import AddressIcon from '../icons/address-icon';
-import DateIcon from '../icons/date-icon';
-import LogisticsItem from './logistics-item';
-import classes from './event-logistics.module.css';
+import React from "react";
+import Image from "next/image";
+import AddressIcon from "../icons/address-icon";
+import DateIcon from "../icons/date-icon";
+import LogisticsItem from "./logistics-item";
+import classes from "./event-logistics.module.css";
 
 function EventLogistics(props) {
   const { date, address, image, imageAlt } = props;
 
-  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
-  const addressText = address.replace(', ', '\n');
+  const addressText = address.replace(", ", "\n");
+
+	// it's a dummy loader, in real app this loader will actually do some optimization
+	// and it will probably set up in next.config.js to make adjustment global
+  const loader = () => image;
+	console.log(image)
 
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image
+          loader={loader}
+          src={image}
+          alt={imageAlt}
+          width={400}
+          height={400}
+        />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
--- 05 - Next.js optimization/components/events/event-item.js
+++ 05 - Next.js optimization/components/events/event-item.js
@@ -1,25 +1,39 @@
-import Link from 'next/link';
-import Button from '../ui/button';
-import DateIcon from '../icons/date-icon';
-import AddressIcon from '../icons/address-icon';
-import ArrowRightIcon from '../icons/arrow-right-icon';
-import classes from './event-item.module.css';
+import Link from "next/link";
+import Image from "next/image";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
+	console.log(image)
 
-  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
-  const formattedAddress = location.replace(', ', '\n');
+  const formattedAddress = location.replace(", ", "\n");
   const exploreLink = `/events/${id}`;
+
+	// it's a dummy loader, in real app this loader will actually do some optimization
+	// and it will probably set up in next.config.js to make adjustment global
+  const loader = () => image;
 
   return (
     <li className={classes.item}>
       <Link href={exploreLink}>
-        <img src={'/' + image} alt={title} className={classes.itemLink} />
+        <a>
+          <Image
+            loader={loader}
+            src={image}
+            alt={title}
+            width={250}
+            height={160}
+          />
+        </a>
       </Link>
       <div className={classes.content}>
         <div className={classes.summary}>

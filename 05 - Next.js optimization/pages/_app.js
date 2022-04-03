13d0a7bb44022296a340328297fc65fac922cd95
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-04-03 20:09:27 +0300

    added title, description and meta to head

--- 05 - Next.js optimization/pages/_app.js
+++ 05 - Next.js optimization/pages/_app.js
@@ -1,9 +1,19 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import React from "react";
+import Layout from "../components/layout/layout";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+				<title>NextJS Event</title>
+				<meta
+					name='description'
+					content='Find a lot of great NextJS events'
+				/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
       <Component {...pageProps} />
     </Layout>
   );

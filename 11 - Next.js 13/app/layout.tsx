772a13714fa91cc48b08f55b953dc9c73a1047db
khnychkin_kirill <kirill.khnychkin@gmail.com>
2023-01-05 00:53:30 +0400

    added navbar and global styles

--- 11 - Next.js 13/app/layout.tsx
+++ 11 - Next.js 13/app/layout.tsx
@@ -1,12 +1,26 @@
+import Link from "next/link";
+import './styles.css';
+
 export default function RootLayout({
-  children,
+ children,
 }: {
   children: React.ReactNode
 }) {
   return (
     <html>
-      <head />
-      <body>{children}</body>
+    <head/>
+
+    <body>
+    <nav>
+      <Link href="/">
+        Home
+      </Link>
+      <Link href="/cards">
+        Notes
+      </Link>
+    </nav>
+    {children}
+    </body>
     </html>
   )
 }
